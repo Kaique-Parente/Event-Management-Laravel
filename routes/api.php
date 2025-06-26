0569ef8ac12011d6837b3ce7c4b1b372cf8c12ac
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-26 17:48:20 -0300

    [ADD] Rotas de API configuradas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AttendController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+
+Route::apiResource('events.attendees', AttendController::class)
+    ->scoped(['attend'=> 'events']);

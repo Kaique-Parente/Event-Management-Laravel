6d6661dafdc2824e5df5ce5c2d4b7613cbfffd58
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-27 12:57:22 -0300

    [ADD] API Store event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -21,7 +21,17 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required',
+            'end_time' => 'required|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
